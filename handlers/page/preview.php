2a810a82d5b98fb53bbc1d8f8b6149143450291a
Thomas Parisot <oncletom@users.noreply.github.com>
2020-08-05 19:06:28 +0200

    Hide link URLs for now
    Maybe display them for external websites?

--- handlers/page/preview.php
+++ handlers/page/preview.php
@@ -6,16 +6,23 @@ if ($wiki->HasAccess('read') && isset($wiki->page['metadatas']['publication-titl
     include_once 'includes/squelettephp.class.php';
     $exportTemplate = new SquelettePhp('print-preview.tpl.html', 'publication');
 
+    $themeCustomCSS = 'themes/' . $wiki->config['favorite_theme'] . '/styles/publication.override.css';
+
     echo $exportTemplate->render(array(
         "baseUrl" => $wiki->getBaseUrl(),
         "content" => $wiki->Format($wiki->page["body"]),
         "siteTitle" => $wiki->GetConfigValue('wakka_name'),
         "title" => $wiki->page['metadatas']['publication-title'],
         "styles" => $wiki->Format("{{linkstyle}}"),
+        "themeCustomCSS" => file_exists($themeCustomCSS) ? $themeCustomCSS : '',
         "stylesModifiers" => array(
             "print",
+            // could be chosen, when creating an eBook
             "page-format--A4",
-            "page-orientation--portrait"
+            // could be chosen when creating an eBook
+            "page-orientation--portrait",
+            // could be optional, per eBook
+            "hide-links-from-print",
         ),
     ));
 }

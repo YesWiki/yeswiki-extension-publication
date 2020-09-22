5b3ed317993a18122d48ecf29beba982b89682af
Thomas Parisot <oncletom@users.noreply.github.com>
2020-09-22 23:49:07 +0200

    Display a sneak peak of a publication instead of its rendered HTML
    refs #18

---
+++ handlers/page/show__.php
@@ -0,0 +1,29 @@
+<?php
+global $wiki;
+
+if ($this->HasAccess('write') && isset($this->page['metadatas']['publication-title'])) {
+    include_once 'includes/squelettephp.class.php';
+
+    // user  options
+    $options = array(
+        "publication-hide-links-url" => '1',
+        "publication-cover-page" => '0',
+        "publication-book-fold" => '0',
+        "publication-page-format" => 'A4',
+        "publication-page-orientation" => 'portrait',
+        "publication-pagination" => "bottom-center",
+        "publication-print-marks" => '0'
+    );
+
+    $metadata = array_merge($options, $wiki->page['metadatas']);
+    $template = new SquelettePhp('print-show.tpl.html', 'publication');
+
+    $output = $template->render(array(
+      'hasWriteAccess' => $wiki->HasAccess('write'),
+      'metadata' => $metadata,
+      'page' => $wiki->page,
+      'wiki' => $wiki,
+    ));
+
+    $plugin_output_new = preg_replace('/<div class="page" >.+<hr class="hr_clear" \/>/siU', '<div class="page" >'. $output .'<hr class="hr_clear" />', $plugin_output_new);
+}

d2e31fe3fe552ee3d908d7ef48c1390c70616281
srosset81 <srosset81@gmail.com>
2020-10-26 18:52:38 +0100

    Initialize services

--- actions/publicationgenerator.php
+++ actions/publicationgenerator.php
@@ -16,7 +16,7 @@ if (!defined("WIKINI_VERSION")) {
     die("acc&egrave;s direct interdit");
 }
 
-global $bazarFiche;
+$ficheManager = $this->services->get('bazar.fiche.manager');
 
 include_once 'tools/tags/libs/tags.functions.php';
 
@@ -124,7 +124,7 @@ if (!empty($groupselector)) {
                     }
                 }
             }
-            $results[$i]['entries'] = $bazarFiche->search(['queries' => $tabQuery, 'formsIds' => [$formId]]);
+            $results[$i]['entries'] = $ficheManager->search(['queries' => $tabQuery, 'formsIds' => [$formId]]);
             $results[$i]['entries'] = searchResultstoArray($results[$i]['entries'], array(), $formValues);
             // tri des fiches
             $GLOBALS['ordre'] = 'asc';
@@ -186,7 +186,7 @@ if (!empty($groupselector)) {
     // bazar entries
     $results[1]['type'] = 'bazar';
     $results[1]['name'] = 'Fiches bazar';
-    $results[1]['entries'] = $bazarFiche->search();
+    $results[1]['entries'] = $ficheManager->search();
     $GLOBALS['ordre'] = 'asc';
     $GLOBALS['champ'] = 'bf_titre';
     usort($results[1]['entries'], 'champCompare');
@@ -259,7 +259,7 @@ if (isset($_POST["page"])) {
 			$acceptedTags = '<h1><h2><h3><h4><h5><h6><hr><hr/><br><br/><span><blockquote><i><u><b><strong><ol><ul><li><small><div><p><a><table><tr><th><td><img><figure><caption><iframe>';
 			$fiche['bf_content'] = strip_tags($fiche['bf_content'], $acceptedTags);
             $fiche['antispam'] = 1;
-            $fiche = $bazarFiche->create($formId, $fiche);
+            $fiche = $ficheManager->create($formId, $fiche);
 			$output = $this->Format('""<div class="alert alert-success">' . _t('PUBLICATION_NEWSLETTER_CREATED') . ' !""' . "\n" . '{{button class="btn-primary" link="' . $fiche['id_fiche'] . '" text="' . _t('PUBLICATION_SEE_NEWSLETTER') . ' ' . $fiche['bf_titre'] . '"}}""</div>""' . "\n");
 		}
 	} while (FALSE); // End of global do-while loop

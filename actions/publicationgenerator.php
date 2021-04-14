c46868475e896624841609484a0beb024f6c9c4f
Florian Schmitt <mrflos@gmail.com>
2021-04-14 11:29:49 +0300

    fix(generator): use strcasecmp + psr2 indentation

--- actions/publicationgenerator.php
+++ actions/publicationgenerator.php
@@ -31,24 +31,24 @@ $outputFormat = $this->getParameter('outputformat', 'Ebook');
 $messages = [];
 
 if ($outputFormat === 'newsletter') {
-	$formId = $this->getParameter('formid'); // Bazar form used to store the newsletter
-	if (empty($formId)) {
-		exit (_t('PUBLICATION_MISSING_NEWSLETTER_FORM'));
-	} else {
-		$name = _t('PUBLICATION_NEWSLETTER');
-	}
+    $formId = $this->getParameter('formid'); // Bazar form used to store the newsletter
+    if (empty($formId)) {
+        exit(_t('PUBLICATION_MISSING_NEWSLETTER_FORM'));
+    } else {
+        $name = _t('PUBLICATION_NEWSLETTER');
+    }
 }
 
 // Deprecated arguments
 // In the form of ['oldvalue', 'newvalue']
 $messages = array_reduce([['ebookpagenameprefix', 'pagenameprefix'], ['fields', 'readonly'], ['ebookstart', 'pagestart'], ['ebookend', 'pageend'], ['publicationstart', 'pagestart'], ['publicationend', 'pageend']], function ($accumulator, $item) {
-  list($key, $expectation) = $item;
+    list($key, $expectation) = $item;
 
-  if ($this->getParameter($key, null) !== null) {
-    array_push($accumulator, ['warning', sprintf(_t('PUBLICATION_PARAMETER_DEPRECATED'), $key, $expectation)]);
-  }
+    if ($this->getParameter($key, null) !== null) {
+        array_push($accumulator, ['warning', sprintf(_t('PUBLICATION_PARAMETER_DEPRECATED'), $key, $expectation)]);
+    }
 
-  return $accumulator;
+    return $accumulator;
 }, $messages);
 
 // Indicates if fields and elements are "read only"
@@ -210,114 +210,112 @@ $output = '';
 // Handling of data submitted by the form
 // Page creation
 if (isset($_POST["page"])) {
-	do { // use of a do-while loop in order to allow for breaks (in case of errors)
-		if (!isset($_POST['antispam']) || $_POST['antispam'] != 1) {
-			// There may be a spamming problem
-			$output = '<div class="alert alert-danger">' . _t('PUBLICATION_SPAM_RISK') . '</div>' . "\n";
-			break; // Stops the current do-while loop
-		}
-		if (!isset($_POST["publication-title"]) || $_POST["publication-title"] == '') {
-			// There is no publication-title
-			$output = '<div class="alert alert-danger">' . _t('PUBLICATION_NO_TITLE_FOUND') . '</div>' . "\n";
-			break; // Stops the current do-while loop
+    do { // use of a do-while loop in order to allow for breaks (in case of errors)
+        if (!isset($_POST['antispam']) || $_POST['antispam'] != 1) {
+            // There may be a spamming problem
+            $output = '<div class="alert alert-danger">' . _t('PUBLICATION_SPAM_RISK') . '</div>' . "\n";
+            break; // Stops the current do-while loop
+        }
+        if (!isset($_POST["publication-title"]) || $_POST["publication-title"] == '') {
+            // There is no publication-title
+            $output = '<div class="alert alert-danger">' . _t('PUBLICATION_NO_TITLE_FOUND') . '</div>' . "\n";
+            break; // Stops the current do-while loop
         }
 
-		// So far everything is OK
-		if (strtolower($_POST['outputformat']) == 'ebook') {  // We want to produce an ebook (default behaviour)
-			do { // use of a do-while loop in order to allow for breaks (in case of errors specific to ebooks)
-			  if (isset($_POST["publication-cover-image"]) && $_POST["publication-cover-image"] !== '' && preg_match("/.(jpe?g|png|svg)$/i", $_POST["publication-cover-image"]) != 1) {
-					// there is no publication-cover-image
-					$output = '<div class="alert alert-danger">' . _t('PUBLICATION_NOT_IMAGE_FILE') . '</div>' . "\n";
-					break; // Stops the current do-while loop
-				}
-				// So far everything is OK (regarding ebooks)
-				if (isset($ebookPageName) && !empty($ebookPageName)) {
-					$pageName = $ebookPageName;
-				} else {
-					$pageName = generatePageName($ebookPageNamePrefix . ' ' . $_POST["publication-title"]);
+        // So far everything is OK
+        if (strcasecmp($_POST['outputformat'], 'ebook') == 0) {  // We want to produce an ebook (default behaviour)
+            do { // use of a do-while loop in order to allow for breaks (in case of errors specific to ebooks)
+              if (isset($_POST["publication-cover-image"]) && $_POST["publication-cover-image"] !== '' && preg_match("/.(jpe?g|png|svg)$/i", $_POST["publication-cover-image"]) != 1) {
+                  // there is no publication-cover-image
+                  $output = '<div class="alert alert-danger">' . _t('PUBLICATION_NOT_IMAGE_FILE') . '</div>' . "\n";
+                  break; // Stops the current do-while loop
+              }
+                // So far everything is OK (regarding ebooks)
+                if (isset($ebookPageName) && !empty($ebookPageName)) {
+                    $pageName = $ebookPageName;
+                } else {
+                    $pageName = generatePageName($ebookPageNamePrefix . ' ' . $_POST["publication-title"]);
+                }
+
+
+                // Generate the content of the page body
+                // @todo refactor it to share its logic with newsletter
+                foreach ($_POST["page"] as $page) {
+                    // we turn some actions into explicit content
+                    // for now, {{blankpage}}, but later maybe some specific handlers like {{publicationcover}}, {{publicationbookend}}
+                    if (preg_match('#{{\s*blankpage\s*}}#U', $page)) {
+                        $output .= $page . "\n";
+                    }
+                    // we assume it is a page tag otherwise
+                    // maybe we should also explicitly check it is a valid page tag instead?
+                    // $page can be 'SomeTag' or 'SomeTag?parameter=value'
+                    // the query string is used to parametrize book creation
+                    else {
+                        list($page, $qs) = explode('?', $page);
+                        parse_str($qs, $qs);
+
+                        $output .= '{{include page="' . $page . '" class="'. trim(implode(' ', [$qs['type'], $qs['class']])) .'"}}' . "\n";
+                    }
+                }
+
+                unset($_POST['page']);
+                unset($_POST['antispam']);
+
+                if ($this->SavePage($pageName, $output) === 0) {
+                    $this->SaveMetaDatas($pageName, $_POST);
+                    $this->SetMessage(_t('PUBLICATION_EBOOK_PAGE_CREATED'));
+                    $this->Redirect($this->Href('', $pageName));
+                    exit();
+                } else {
+                    $output = $this->Format('""<div class="alert alert-danger alert-error">' . _t('PUBLICATION_EBOOK_PAGE_CREATION_FAILED') . '.""' . "\n" . '{{button class="btn-primary" link="' . $this->GetPageTag() . '" text="' . _t('PUBLICATION_GOTO_EBOOK_CREATION_PAGE') . ' ' . $this->GetPageTag() . '"}}""</div>""' . "\n");
+                }
+            } while (false); // end of ebook specific loop
+        } elseif (strcasecmp($outputFormat, 'newsletter') == 0) { // We want to produce a newsletter
+            $fiche['id_typeannonce'] = $formId;
+            $fiche['bf_titre'] = $_POST["publication-title"];
+            $fiche['bf_description'] = $_POST["publication-description"];
+            $fiche['bf_author'] = $_POST["publication-author"];
+            $fiche['bf_content'] = '';
+
+            // Generate the content of the page body
+            // @todo Refactor this as a function to share it with Ebook logic
+            foreach ($_POST["page"] as $page) {
+                // we turn some actions into explicit content
+                // for now, {{blankpage}}, but later maybe some specific handlers like {{publicationcover}}, {{publicationbookend}}
+                if (preg_match('#{{\s*blankpage\s*}}#U', $page)) {
+                    $fiche['bf_content'] .= $this->Format($page . "\n");
+                }
+                // we assume it is a page tag otherwise
+                // maybe we should also explicitly check it is a valid page tag instead?
+                else {
+                    $fiche['bf_content'] .= $this->Format('{{include page="' . $page . '" class=""}}' . "\n");
+                }
+            }
+
+            $acceptedTags = '<h1><h2><h3><h4><h5><h6><hr><hr/><br><br/><span><blockquote><i><u><b><strong><ol><ul><li><small><div><p><a><table><tr><th><td><img><figure><caption><iframe>';
+            $fiche['bf_content'] = strip_tags($fiche['bf_content'], $acceptedTags);
+            $fiche['antispam'] = 1;
+            $fiche = $entryManager->create($formId, $fiche);
+            array_push($messages, array('success', _t('PUBLICATION_NEWSLETTER_CREATED')));
         }
-
-
-        // Generate the content of the page body
-        // @todo refactor it to share its logic with newsletter
-        foreach ($_POST["page"] as $page) {
-          // we turn some actions into explicit content
-          // for now, {{blankpage}}, but later maybe some specific handlers like {{publicationcover}}, {{publicationbookend}}
-          if (preg_match('#{{\s*blankpage\s*}}#U', $page)) {
-            $output .= $page . "\n";
-          }
-          // we assume it is a page tag otherwise
-          // maybe we should also explicitly check it is a valid page tag instead?
-          // $page can be 'SomeTag' or 'SomeTag?parameter=value'
-          // the query string is used to parametrize book creation
-          else {
-            list($page, $qs) = explode('?', $page);
-            parse_str($qs, $qs);
-
-            $output .= '{{include page="' . $page . '" class="'. trim(implode(' ', [$qs['type'], $qs['class']])) .'"}}' . "\n";
-          }
-        }
-
-				unset($_POST['page']);
-        unset($_POST['antispam']);
-
-        if ($this->SavePage($pageName, $output) === 0) {
-          $this->SaveMetaDatas($pageName, $_POST);
-          $this->SetMessage(_t('PUBLICATION_EBOOK_PAGE_CREATED'));
-          $this->Redirect($this->Href('', $pageName));
-          exit();
-        }
-        else {
-          $output = $this->Format('""<div class="alert alert-danger alert-error">' . _t('PUBLICATION_EBOOK_PAGE_CREATION_FAILED') . '.""' . "\n" . '{{button class="btn-primary" link="' . $this->GetPageTag() . '" text="' . _t('PUBLICATION_GOTO_EBOOK_CREATION_PAGE') . ' ' . $this->GetPageTag() . '"}}""</div>""' . "\n");
-        }
-
-			} while (FALSE); // end of ebook specific loop
-		} elseif ($outputFormat == 'newsletter') { // We want to produce a newsletter
-			$fiche['id_typeannonce'] = $formId;
-			$fiche['bf_titre'] = $_POST["publication-title"];
-			$fiche['bf_description'] = $_POST["publication-description"];
-			$fiche['bf_author'] = $_POST["publication-author"];
-      $fiche['bf_content'] = '';
-
-      // Generate the content of the page body
-      // @todo Refactor this as a function to share it with Ebook logic
-			foreach ($_POST["page"] as $page) {
-        // we turn some actions into explicit content
-        // for now, {{blankpage}}, but later maybe some specific handlers like {{publicationcover}}, {{publicationbookend}}
-        if (preg_match('#{{\s*blankpage\s*}}#U', $page)) {
-          $fiche['bf_content'] .= $this->Format($page . "\n");
-        }
-        // we assume it is a page tag otherwise
-        // maybe we should also explicitly check it is a valid page tag instead?
-        else {
-  				$fiche['bf_content'] .= $this->Format('{{include page="' . $page . '" class=""}}' . "\n");
-        }
-      }
-
-			$acceptedTags = '<h1><h2><h3><h4><h5><h6><hr><hr/><br><br/><span><blockquote><i><u><b><strong><ol><ul><li><small><div><p><a><table><tr><th><td><img><figure><caption><iframe>';
-			$fiche['bf_content'] = strip_tags($fiche['bf_content'], $acceptedTags);
-      $fiche['antispam'] = 1;
-      $fiche = $entryManager->create($formId, $fiche);
-      array_push($messages, array('success', _t('PUBLICATION_NEWSLETTER_CREATED')));
-		}
-	} while (FALSE); // End of global do-while loop
+    } while (false); // End of global do-while loop
 } else { // Not isset($_POST["page"])
     // recuperation des pages creees a l'installation
     $installedPageNames = [];
     if (!empty($addInstalledPages)) {
-      $d = dir("setup/doc/");
-      while ($doc = $d->read()) {
-          if ($doc == '.' || $doc == '..' || is_dir($doc) || substr($doc, -4) != '.txt') {
-              continue;
-          }
+        $d = dir("setup/doc/");
+        while ($doc = $d->read()) {
+            if ($doc == '.' || $doc == '..' || is_dir($doc) || substr($doc, -4) != '.txt') {
+                continue;
+            }
 
-          if ($doc == '_root_page.txt') {
-              $installedPageNames[$this->GetConfigValue("root_page")] = $this->GetConfigValue("root_page");
-          } else {
-              $pageName = substr($doc, 0, strpos($doc, '.txt'));
-              $installedPageNames[$pageName] = $pageName;
-          }
-      }
+            if ($doc == '_root_page.txt') {
+                $installedPageNames[$this->GetConfigValue("root_page")] = $this->GetConfigValue("root_page");
+            } else {
+                $pageName = substr($doc, 0, strpos($doc, '.txt'));
+                $installedPageNames[$pageName] = $pageName;
+            }
+        }
     }
 
     if (isset($this->page["metadatas"]["publication-title"])) {
@@ -345,7 +343,7 @@ if (isset($_POST["page"])) {
     $exportTemplate = new SquelettePhp($template, 'publication');
 
     $this->AddJavascriptFile('tools/publication/libs/vendor/jquery-ui-sortable/jquery-ui.min.js');
-  	$this->AddJavascriptFile('tools/publication/presentation/actions/publicationgenerator.js');
+    $this->AddJavascriptFile('tools/publication/presentation/actions/publicationgenerator.js');
 
     $output .= $exportTemplate->render(array(
       'messages' => $messages,

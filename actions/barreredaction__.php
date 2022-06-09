ef63c3f7164ec287876781c7d7245ee1c444b193
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-06-09 18:15:54 +0200

    fix(pdf button): use shorter text

--- actions/barreredaction__.php
+++ actions/barreredaction__.php
@@ -3,4 +3,4 @@ if (!defined("WIKINI_VERSION")) {
     die("acc&egrave;s direct interdit");
 }
 
-$plugin_output_new = preg_replace('#</div>#', '<a class="link-pdf" href="'.$this->href('pdf').'" title="'._t('PUBLICATION_EXPORT_PAGE_TO_PDF').'"><i class="glyphicon glyphicon-book"></i> '. _t('PUBLICATION_EXPORT_PAGE_TO_PDF') .'</a>'."\n".'</div>', $plugin_output_new);
+$plugin_output_new = preg_replace('#</div>#', '<a class="link-pdf" href="'.$this->href('pdf').'" title="'._t('PUBLICATION_EXPORT_PAGE_TO_PDF').'"><i class="glyphicon glyphicon-book"></i> '. _t('PUBLICATION_DOWNLOAD_PDF') .'</a>'."\n".'</div>', $plugin_output_new);

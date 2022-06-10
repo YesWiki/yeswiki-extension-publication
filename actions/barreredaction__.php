26e5791ece5187c37e09b853d07fa89771bfce4c
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-06-10 15:42:43 +0200

    fix(/pdf): add toast message when asking print
    fix #53

--- actions/barreredaction__.php
+++ actions/barreredaction__.php
@@ -1,6 +1,7 @@
 <?php
+
 if (!defined("WIKINI_VERSION")) {
     die("acc&egrave;s direct interdit");
 }
 
-$plugin_output_new = preg_replace('#</div>#', '<a class="link-pdf" href="'.$this->href('pdf').'" title="'._t('PUBLICATION_EXPORT_PAGE_TO_PDF').'"><i class="glyphicon glyphicon-book"></i> '. _t('PUBLICATION_DOWNLOAD_PDF') .'</a>'."\n".'</div>', $plugin_output_new);
+$plugin_output_new = preg_replace('#</div>#', '<a class="link-pdf" href="'.$this->href('pdf').'" title="'._t('PUBLICATION_EXPORT_PAGE_TO_PDF').'" onclick="toastMessage(_t(\'PUBLICATION_PDF_GENERATION_LANCHED\'),7000,\'alert alert-primary\');"><i class="glyphicon glyphicon-book"></i> '. _t('PUBLICATION_DOWNLOAD_PDF') .'</a>'."\n".'</div>', $plugin_output_new);

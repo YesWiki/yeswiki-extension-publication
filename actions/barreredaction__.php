b2bc41c1c1a38e871d97644e5d4bd5b5b9625c32
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-11-15 16:30:36 +0100

    fix(barreredaction): use _publication_button.twig

--- actions/barreredaction__.php
+++ actions/barreredaction__.php
@@ -4,4 +4,4 @@ if (!defined("WIKINI_VERSION")) {
     die("acc&egrave;s direct interdit");
 }
 
-$plugin_output_new = preg_replace('#</div>#', '<a class="link-pdf" href="'.$this->href('pdf').'" title="'._t('PUBLICATION_EXPORT_PAGE_TO_PDF').'" onclick="toastMessage(_t(\'PUBLICATION_PDF_GENERATION_LANCHED\'),7000,\'alert alert-primary\');"><i class="glyphicon glyphicon-book"></i> '. _t('PUBLICATION_DOWNLOAD_PDF') .'</a>'."\n".'</div>', $plugin_output_new);
+$plugin_output_new = preg_replace('#</div>#', $this->render('@bazar/entries/_publication_button.twig', ['forPage'=>true])."\n".'</div>', $plugin_output_new);
